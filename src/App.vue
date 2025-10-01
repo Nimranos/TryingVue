008ffdc388a5152c18550e3160df548f00311511
Nimranos <nimcze@gmail.com>
2025-10-01 11:39:52 +0200

    Tailwind, Navbar, Hero template, Props test

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,9 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-
-const name = ref('John Doe');
-const status = ref('active');
-const tasks = ref(['Task One', 'Task Two', 'Task Three', 'Task Four']);
-const link = ref('https://google.com');
-const newTask = ref('');
-
-const toggleStatus = () => {
-  if (status.value === 'active') {
-    status.value = 'pending';
-  } else if (status.value === 'pending') {
-    status.value = 'inactive';
-  } else {
-    status.value = 'active';
-  };
-};
-
-const addTask = () =>{
-if(newTask.value.trim() !== ''){
-  tasks.value.push(newTask.value);
-  newTask.value = '';
-};
-};
-const deleteTask = (index) =>{
-  tasks.value.splice(index, 1)
-};
-
-onMounted(async ()=>{
-  try{
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-    const data = await response.json();
-    tasks.value = data.map((task)=> task.title);
-  }catch (error){
-    console.log('error fetching tasks');
-  }
-});
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
 </script>
 
 <template>
-  <h1>{{name}}</h1>
-  <p v-if="status === 'active'">user is active</p>
-  <p v-else-if="status === 'pending'">user is pending</p>
-  <p v-else>user is inactive</p>
-
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <input type="text" id="newTask" name="NewTask" v-model="newTask"/>
-    <button type="submit">Submit</button>
-  </form>
-
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-for="(task,index) in tasks" :key="task">
-      <span>
-        {{ task }}
-      </span>
-      <button @click="deleteTask(index)">X</button>
-    </li>
-  </ul>
-  <!-- <a v-bind:href="link">Click for google link</a> -->
-<br/>
-  <button @click="toggleStatus">Change status</button>
+<Navbar/>
+<Hero title="Test Title" subtitle="Test Subtitle"/>
 </template>
